c3e758c05e2af80464b1529cd2794f4d396af7c6
Serkan Celen <serkancelen@outlook.com.tr>
2024-02-12 13:35:30 +0300

    Added Services

---
+++ Services/Contracts/ISkillsService.cs
@@ -0,0 +1,13 @@
+﻿using Entities.ModelsDto;
+
+namespace Service.Contracts
+{
+	public interface ISkillsService
+	{
+		List<SkillsDto> GetAll();
+		SkillsDto GetById(int id);
+		void Add(SkillsDto dto);
+		void Update(SkillsDto dto);
+		void Delete(int id);
+	}
+}
---
+++ Services/GalleryService.cs
@@ -0,0 +1,69 @@
+﻿using Entities.Models;
+using Entities.ModelsDto;
+using Service.Contracts;
+
+namespace Service
+{
+	public class GalleryService : IGalleryService
+	{
+		private readonly ApplicationDbContext _context;
+
+		public GalleryService(ApplicationDbContext context)
+		{
+			_context = context;
+		}
+
+		public List<GalleryDto> GetAll()
+		{
+			return _context.Galleries.Select(g => new GalleryDto
+			{
+				GalleryId = g.GalleryId,
+				Image = g.Image
+			}).ToList();
+		}
+
+		public GalleryDto GetById(string id)
+		{
+			var galleryItem = _context.Galleries.Find(id);
+			return new GalleryDto
+			{
+				GalleryId = galleryItem?.GalleryId,
+				Image = galleryItem?.Image
+			};
+		}
+
+		public void Add(GalleryDto dto)
+		{
+			var entity = new Gallery
+			{
+				GalleryId = dto.GalleryId,
+				Image = dto.Image
+			};
+
+			_context.Galleries.Add(entity);
+			_context.SaveChanges();
+		}
+
+		public void Update(GalleryDto dto)
+		{
+			var entity = _context.Galleries.Find(dto.GalleryId);
+
+			if (entity != null)
+			{
+				entity.Image = dto.Image;
+				_context.SaveChanges();
+			}
+		}
+
+		public void Delete(string id)
+		{
+			var entity = _context.Galleries.Find(id);
+
+			if (entity != null)
+			{
+				_context.Galleries.Remove(entity);
+				_context.SaveChanges();
+			}
+		}
+	}
+}
