64dcd19c396f1a4f5deb7f19d3425146e047cc43
kdrkrc19 <kadircankaraca01@gmail.com>
2024-02-14 11:21:04 +0300

    UserDto düzenlendi

--- Repositories/Contracts/IRepositoryAboutUs.cs
+++ Repositories/Contracts/IRepositoryAboutUs.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Repositories.EFCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Repositories.Contracts
 {
-    public interface IRepositoryAboutUs
     {
+        IQueryable<AboutUs> GetAboutUs(int id, bool trackchanges);
     }
 }
